37b324bf47c4d6524762183d7ba13b83875a4366
Yusuke_Kanazawa <c1005073@planet.kanazawa-it.ac.jp>
2025-10-23 16:55:07 +0900

    pcl_rosに置換用のCMakeLists.txt追加

--- tools/pointcloud_to_pcd.sh
+++ tools/pointcloud_to_pcd.sh
@@ -3,5 +3,7 @@
 ros2 run pcl_ros pointcloud_to_pcd --ros-args \
     -r input:=/livox/lidar \
     -p fixed_frame:=livox_frame \
-    -p prefix:=/path/to/your/output/dir/path/abc_
+    -p prefix:=/home/demulab-kohei/tmp/
+
+#/path/to/your/output/dir/path/filename_
 
